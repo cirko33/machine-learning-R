6319e812d94efd06c61cf56cc2e5642ee672b2ec
cirko33 <luka.ciric2106@gmail.com>
2024-06-03 11:44:15 +0200

    Works like a charm

--- script.r
+++ script.r
@@ -13,18 +13,29 @@ library(pandoc)
 # Install
 spark_install()
 
-# Connect
-sc <- spark_connect(master = "local")
+conf <- spark_config()
+conf["spark.executor.memory"] <- "6G"
+conf["sparklyr.shell.driver-memory"] <- "6G"
+sc <- spark_connect(master = "local", config = conf)
 
 
 ds.path <- "~/FAX/RVPUII/projekat/data"
-ds.df <- spark_read_csv(sc, name = "my_data", path = ds.path, header = TRUE, infer_schema = TRUE)
+ds.df <- spark_read_csv(sc, name = "my_data", path = ds.path, header = TRUE, infer_schema = TRUE, memory = TRUE)
 ds.filtered <- ds.df %>% 
-    select(-airport_fee, -congestion_surcharge)  %>%
-    filter_all(any_vars(is.na(.)))
+    select(-airport_fee, -congestion_surcharge) %>%
+    filter(
+      !is.na(VendorID) || !is.na(tpep_pickup_datetime) || !is.na(tpep_dropoff_datetime) || !is.na(passenger_count) || 
+      !is.na(trip_distance) || !is.na(RatecodeID) || !is.na(store_and_fwd_flag) || !is.na(PULocationID) || 
+      !is.na(DOLocationID) || !is.na(payment_type) || !is.na(fare_amount) || !is.na(extra) || !is.na(mta_tax) || 
+      !is.na(tip_amount) || !is.na(tolls_amount) || !is.na(improvement_surcharge) || !is.na(total_amount)
+    )
 
-# Training data
-ds.data <- ds.filtered %>% sdf_partition(train = 0.8, test = 0.2, seed = 1099)
+data <- ds.filtered %>% 
+    mutate(received_tip = ifelse(tip_amount > 0, T, F))
+
+
+# Every attribute represented
+VendorIDs <- pull(data, VendorID)
 
 # Classification models
 model.first <- tip_amount ~ trip_distance + passenger_count + total_amount + VendorID + RatecodeID
@@ -32,8 +43,11 @@ model.second <- tip_amount ~ trip_distance + payment_type + mta_tax + improvemen
 model.third <- tip_amount ~ tpep_pickup_datetime + tpep_dropoff_datetime + passenger_count + RatecodeID + store_and_fwd_flag
 
 # Linear regression
-classification.lr.first <- ds.data$train %>%
-    ml_linear_regression(model.first)
+classification.lr.first <- lm(model.first, data=ds.filtered)
+summary(classification.lr.first)
+predictions.lr.first <- predict(classification.lr.first, select(ds.filtered[1,], trip_distance, passenger_count, total_amount, VendorID, RatecodeID))
+predictions.lr.first
+
 
 classification.lr.second <- ds.data$train %>%
     ml_linear_regression(model.second)
@@ -41,11 +55,9 @@ classification.lr.second <- ds.data$train %>%
 classification.lr.third <- ds.data$train %>%
     ml_linear_regression(model.third)
 
-predictions.lr.first <- classification.lr.first %>%
-    ml_predict(ds.data$test)
 
 predictions.lr.second <- classification.lr.second %>%
-    ml_predict(ds.data$test)
+    ml_predi2ct(ds.data$test)
 
 predictions.lr.third <- classification.lr.third %>%
     ml_predict(ds.data$test)
